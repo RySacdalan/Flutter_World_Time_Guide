2983fb8cb9319e34e91808bdd34bbe46d03aa19b
developer@trashcash.ph <sacdalanryanchristian6404@gmail.com>
2022-01-02 11:19:32 +0800

    Add List Builder & image icons.

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_world_time/services/world_time.dart';
 
 class ChooseLocation extends StatefulWidget {
   const ChooseLocation({Key? key}) : super(key: key);
@@ -8,6 +9,19 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
+  //data of all the WorldTime
+  List<WorldTime> location = [
+    WorldTime(url: 'Europe/London', location: 'London', flag: 'uk.png'),
+    WorldTime(url: 'Europe/Berlin', location: 'Athens', flag: 'greece.png'),
+    WorldTime(url: 'Africa/Cairo', location: 'Cairo', flag: 'egypt.png'),
+    WorldTime(url: 'Africa/Nairobi', location: 'Nairobi', flag: 'kenya.png'),
+    WorldTime(url: 'America/Chicago', location: 'Chicago', flag: 'usa.png'),
+    WorldTime(url: 'America/New_York', location: 'New York', flag: 'usa.png'),
+    WorldTime(url: 'Asia/Seoul', location: 'Seoul', flag: 'south_korea.png'),
+    WorldTime(url: 'Asia/Jakarta', location: 'Jakarta', flag: 'indonesia.png'),
+    WorldTime(url: 'Asia/Manila', location: 'Manila', flag: 'philippine.png'),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,7 +32,29 @@ class _ChooseLocationState extends State<ChooseLocation> {
         backgroundColor: Colors.blue[900],
         elevation: 0,
       ),
-      body: const Text("This is a text"),
+      body: ListView.builder(
+        itemCount: location.length,
+        //itemBuilder, build each data a widget to return with.
+        //the index, specify which data index or position on the list
+        itemBuilder: (context, index) {
+          //return a card widget
+          return Padding(
+            padding: const EdgeInsets.symmetric(vertical: 1, horizontal: 4),
+            child: Card(
+              child: ListTile(
+                //onTap similar to onPressed
+                onTap: () {},
+                //get all the location name.
+                title: Text(location[index].location),
+                leading: CircleAvatar(
+                  //location[index] to specify exact country/time
+                  backgroundImage: AssetImage('assets/${location[index].flag}'),
+                ),
+              ),
+            ),
+          );
+        },
+      ),
     );
   }
 }
